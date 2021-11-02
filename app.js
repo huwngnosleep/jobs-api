fdfc9dae01e6205d880f3ea76dfe186cc4e59801
huwngnosleep <thanhhung8210@gmail.com>
2021-11-02 09:55:55 +0700

    added some pakages

--- app.js
+++ app.js
@@ -16,11 +16,22 @@ const authenticateUser = require('./middleware/authentication')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('connect successfully')
 })
 app.use('/api/v1/auth', authRouter)
